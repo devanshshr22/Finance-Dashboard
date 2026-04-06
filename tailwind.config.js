9dadcf4b01004e60b0bec0133483266292fec415
Devansh Shrivastava <devansh.shr22@gmail.com>
2026-04-06 12:49:06 +0530

    Fix Tailwind dark mode for production

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: "class",
+
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
 
   safelist: [
+    "dark",
     "dark:bg-slate-900",
     "dark:bg-slate-800",
     "dark:bg-slate-700",
     "dark:border-slate-700",
+    "dark:border-slate-600",
     "dark:text-white",
+    "dark:text-slate-300",
   ],
 
   theme: {
